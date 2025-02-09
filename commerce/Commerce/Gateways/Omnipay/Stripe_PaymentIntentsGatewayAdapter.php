2a790972487019c19d850a3e62b0e08a27ea5a80
tom <tom@msbit.com.au>
2025-02-09 11:46:44 +1100

    1.2.1369

---
+++ commerce/Commerce/Gateways/Omnipay/Stripe_PaymentIntentsGatewayAdapter.php
@@ -0,0 +1,77 @@
+<?php
+namespace Commerce\Gateways\Omnipay;
+
+use Commerce\Gateways\PaymentFormModels\StripePaymentFormModel;
+use Craft\AttributeType;
+use Craft\BaseModel;
+use Omnipay\Common\Message\AbstractRequest;
+use Omnipay\Stripe\Message\PaymentIntents\AuthorizeRequest;
+
+class Stripe_PaymentIntentsGatewayAdapter extends \Commerce\Gateways\CreditCardGatewayAdapter
+{
+    public function handle()
+	{
+		return 'Stripe_PaymentIntents';
+	}
+
+	public function getPaymentFormModel()
+	{
+		return new StripePaymentFormModel();
+	}
+
+	public function cpPaymentsEnabled()
+	{
+		return true;
+	}
+
+	public function getPaymentFormHtml(array $params)
+	{
+		$defaults = [
+			'paymentMethod' => $this->getPaymentMethod(),
+			'paymentForm'   => $this->getPaymentMethod()->getPaymentFormModel(),
+			'adapter'       => $this
+		];
+
+		$params = array_merge($defaults, $params);
+
+		\Craft\craft()->templates->includeJsFile('https://js.stripe.com/v2/');
+		\Craft\craft()->templates->includeJsResource('lib/jquery.payment'.(\Craft\craft()->config->get('useCompressedJs') ? '.min' : '').'.js');
+
+		return \Craft\craft()->templates->render('commerce/_gateways/_paymentforms/stripe', $params);
+
+	}
+
+    /**
+     * @return array
+     */
+    public function defineAttributes()
+    {
+        // In addition to the standard gateway config, here is some custom config that is useful.
+        $attr = parent::defineAttributes();
+        $attr['publishableKey'] = [AttributeType::String];
+        $attr['publishableKey']['label'] = $this->generateAttributeLabel('publishableKey');
+
+        $attr['includeReceiptEmailInRequests'] = [AttributeType::Bool];
+        $attr['includeReceiptEmailInRequests']['label'] = $this->generateAttributeLabel('includeReceiptEmailInRequests');
+        $this->_booleans[] = 'includeReceiptEmailInRequests';
+
+        return $attr;
+    }
+
+    /**
+     * @param AbstractRequest $request
+     * @param BaseModel $paymentForm
+     *
+     * @return void
+     */
+    public function populateRequest(AbstractRequest $request, BaseModel $paymentForm)
+    {
+        parent::populateRequest($request, $paymentForm);
+
+        /** @var AuthorizeRequest $request */
+        $request->setConfirm(true);
+        if ($this->includeReceiptEmailInRequests) {
+            $request->setReceiptEmail($request->getCard()->getEmail());
+        }
+    }
+}
